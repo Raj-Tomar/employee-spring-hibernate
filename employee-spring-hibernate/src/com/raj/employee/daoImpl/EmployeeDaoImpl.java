f029600c39aa7549f5971e075140f4bc4e8f8e3f
Raj Tomar <rajtomar50@gmail.com>
2016-07-13 22:32:09 +0530

    jdbc.properties changed to hibernate.properties

--- employee-spring-hibernate/src/com/raj/employee/daoImpl/EmployeeDaoImpl.java
+++ employee-spring-hibernate/src/com/raj/employee/daoImpl/EmployeeDaoImpl.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.raj.beans.EmployeeBean;
 import com.raj.employee.dao.EmployeeDao;
@@ -19,21 +19,24 @@ public class EmployeeDaoImpl implements EmployeeDao{
 	 @Autowired
 	 private SessionFactory sessionFactory;
 	 private Session session = null;
+	 private Transaction tx = null;
 	 
 	 private static Logger logger = Logger.getLogger(EmployeeDaoImpl.class);
 
 	@Override
-	@Transactional
 	public String saveOrUpdateEmployee(EmployeeBean employee) {
 		logger.info("saveOrUpdateEmployee daoImpl");
 		String status = "0";
 		try {
-			session = sessionFactory.getCurrentSession();
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
 			if(null == employee.getId()){
 				session.save(employee);
+				tx.commit();
 				status = "1";
 			}else{
 				session.update(employee);
+				tx.commit();
 				status = "1";
 			}
 		} catch (Exception e) {
@@ -44,12 +47,12 @@ public class EmployeeDaoImpl implements EmployeeDao{
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	@Transactional
 	public List<EmployeeBean> getAllEmployee() {
 		logger.info("getAllEmployee daoImpl");
 		List<EmployeeBean> list = null;
 		try{
-			session = sessionFactory.getCurrentSession();
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
 			Query query = session.createQuery("From EmployeeBean");
 			list = query.getResultList();
 			logger.info("Total Employee: "+list.size());
@@ -61,12 +64,12 @@ public class EmployeeDaoImpl implements EmployeeDao{
 	}
 
 	@Override
-	@Transactional
 	public EmployeeBean getEmployeeById(Integer id) {
 		logger.info("getEmployeeById daoImpl");
 		EmployeeBean bean = null;
 		try {
-			session = sessionFactory.getCurrentSession();
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
 			bean = session.get(EmployeeBean.class, id);
 		} catch (Exception e) {
 			logger.error("Exception: "+e.getMessage());
@@ -75,15 +78,16 @@ public class EmployeeDaoImpl implements EmployeeDao{
 	}
 
 	@Override
-	@Transactional
 	public String deleteEmployee(Integer id) {
 		logger.info("deleteEmployee daoImpl");
 		String status = "0";
 		try {
-			session = sessionFactory.getCurrentSession();
+			session = sessionFactory.openSession();
+			tx = session.beginTransaction();
 			EmployeeBean bean = session.load(EmployeeBean.class, id);
 			if(null != bean){
 				session.delete(bean);
+				tx.commit();
 				status = "1";
 			}
 		} catch (Exception e) {
